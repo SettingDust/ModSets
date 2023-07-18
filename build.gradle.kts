a63ee5e737a7f05479b9bf5b010a981876153230
SettingDust <settingdust@gmail.com>
2023-07-18 21:48:34 +0800

    ci: add curseforge publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 @file:Suppress("DSL_SCOPE_VIOLATION", "MISSING_DEPENDENCY_CLASS", "FUNCTION_CALL_EXPECTED", "PropertyName")
 
+import net.darkhax.curseforgegradle.TaskPublishCurseForge
+
 plugins {
     java
     `maven-publish`
@@ -13,6 +15,7 @@ plugins {
 
     alias(libs.plugins.shadow) apply false
     alias(libs.plugins.minotaur)
+    alias(libs.plugins.curseforge)
 }
 
 val mod_version: String by project
@@ -31,20 +34,29 @@ val finalJar by tasks.registering(Jar::class) {
     archiveVersion = "${project.version}"
 }
 
+val publishCurseforge by tasks.registering(TaskPublishCurseForge::class) {
+    apiToken = System.getenv("CURSEFORGE_TOKEN")
+    // https://legacy.curseforge.com/minecraft/mc-mods/mod-sets
+    upload(890349, finalJar) {
+        addModLoader("fabric")
+        addGameVersion("1.19.2")
+        addRequirement("yacl", "fabric-language-kotlin")
+        addOptional("modmenu")
+    }
+}
+
 modrinth {
     token.set(System.getenv("MODRINTH_TOKEN")) // This is the default. Remember to have the MODRINTH_TOKEN environment variable set or else this will fail, or set it to whatever you want - just make sure it stays private!
     projectId.set("mod-sets") // This can be the project ID or the slug. Either will work!
     syncBodyFrom.set(rootProject.file("README.md").readText())
     versionType.set("release") // This is the default -- can also be `beta` or `alpha`
     uploadFile.set(finalJar) // With Loom, this MUST be set to `remapJar` instead of `jar`!
-    gameVersions.addAll(
-        "1.19.2",
-    ) // Must be an array, even with only one version
+    gameVersions.addAll("1.19.2") // Must be an array, even with only one version
     loaders.add("fabric") // Must also be an array - no need to specify this if you're using Loom or ForgeGradle
     dependencies {
         required.version("Ha28R6CL", libs.versions.fabric.language.kotlin.get())
         // https://modrinth.com/mod/yacl
-        required.version("yacl", libs.versions.yacl.get())
+        required.version("yacl", "${libs.versions.yacl.get()}+1.19.2-fabric")
         // https://modrinth.com/mod/kinecraft-serialization
         embedded.version("epmEbmF0", libs.versions.kinecraft.serialization.get())
     }
