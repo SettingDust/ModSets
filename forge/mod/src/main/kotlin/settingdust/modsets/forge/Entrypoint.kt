af585de700196a8facd9d078a7861c316e9dd8ed
SettingDust <settingdust@gmail.com>
2024-09-18 20:09:44 +0800

    feat: use new config api

--- forge/mod/src/main/kotlin/settingdust/modsets/forge/Entrypoint.kt
+++ forge/mod/src/main/kotlin/settingdust/modsets/forge/Entrypoint.kt
@@ -1,5 +1,6 @@
 package settingdust.modsets.forge
 
+import kotlinx.coroutines.runBlocking
 import net.minecraft.network.chat.Component
 import net.minecraftforge.client.ConfigScreenHandler.ConfigScreenFactory
 import net.minecraftforge.fml.ModList
@@ -7,10 +8,10 @@ import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.loading.FMLPaths
 import net.minecraftforge.fml.loading.moddiscovery.ModsFolderLocator
 import settingdust.modsets.ModSets
-import settingdust.modsets.config
+import settingdust.modsets.ModSetsConfig
 import settingdust.modsets.forge.service.ModSetsModLocator
-import settingdust.modsets.game.ModSet
-import settingdust.modsets.game.rules
+import settingdust.modsets.ingame.ModSet
+import settingdust.modsets.ingame.ModSetsIngameConfig
 import thedarkcolour.kotlinforforge.forge.LOADING_CONTEXT
 import kotlin.io.path.div
 
@@ -20,15 +21,16 @@ class Entrypoint {
         // Take from
         // https://github.com/isXander/YetAnotherConfigLib/blob/1.20.x/dev/test-forge/src/main/java/dev/isxander/yacl/test/forge/ForgeTest.java
         LOADING_CONTEXT.registerExtensionPoint(ConfigScreenFactory::class.java) {
-            ConfigScreenFactory { _, parent -> ModSets.rules.createScreen(parent) }
+            ConfigScreenFactory { _, parent -> ModSetsIngameConfig.generateConfigScreen(parent) }
         }
 
         val gameDir = FMLPaths.GAMEDIR.get()
         val modsPath = FMLPaths.MODSDIR.get()
-        val modSets = ModSets.rules.modSets
+        val modSets = ModSetsIngameConfig.modSets
 
-        ModSets.rules.ModSetsRegisterCallbacks += {
-            for ((key, value) in
+        runBlocking {
+            ModSetsIngameConfig.MOD_SET_REGISTER_CALLBACK.collect {
+                for ((key, value) in
                 ModSetsModLocator.directoryModSet.mapValues {
                     ModSet(
                         Component.literal(it.key),
@@ -36,35 +38,36 @@ class Entrypoint {
                         it.value.toMutableSet(),
                     )
                 }) {
-                if (key in modSets)
-                    ModSets.LOGGER.warn("Duplicate mod set with directory name: $key")
-                modSets.putIfAbsent(key, value)
-            }
+                    if (key in modSets)
+                        ModSets.LOGGER.warn("Duplicate mod set with directory name: $key")
+                    modSets.putIfAbsent(key, value)
+                }
 
-            for (mod in ModList.get().mods) {
-                val provider = mod.owningFile.file.provider
-                if (provider !is ModsFolderLocator && provider !is ModSetsModLocator) continue
-                if (mod.modId in modSets)
-                    ModSets.LOGGER.warn("Duplicate mod set with directory name: ${mod.modId}")
-                modSets.putIfAbsent(
-                    mod.modId,
-                    ModSet(
-                        Component.literal(mod.modId),
-                        Component.literal("${mod.displayName} ${mod.modId}@${mod.version}"),
-                        mutableSetOf(mod.modId),
+                for (mod in ModList.get().mods) {
+                    val provider = mod.owningFile.file.provider
+                    if (provider !is ModsFolderLocator && provider !is ModSetsModLocator) continue
+                    if (mod.modId in modSets)
+                        ModSets.LOGGER.warn("Duplicate mod set with directory name: ${mod.modId}")
+                    modSets.putIfAbsent(
+                        mod.modId,
+                        ModSet(
+                            Component.literal(mod.modId),
+                            Component.literal("${mod.displayName} ${mod.modId}@${mod.version}"),
+                            mutableSetOf(mod.modId),
+                        )
                     )
-                )
-            }
+                }
 
-            ModSetsConfig.disabledMods.forEach {
-                modSets.putIfAbsent(
-                    it,
-                    ModSet(
-                        Component.literal(it),
-                        Component.literal("$it@disabled"),
-                        mutableSetOf(it),
+                ModSetsConfig.disabledMods.forEach {
+                    modSets.putIfAbsent(
+                        it,
+                        ModSet(
+                            Component.literal(it),
+                            Component.literal("$it@disabled"),
+                            mutableSetOf(it),
+                        )
                     )
-                )
+                }
             }
         }
     }
